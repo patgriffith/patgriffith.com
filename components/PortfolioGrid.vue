243b28f8874495992b5ed36876d66adb2222a6e9
Patrick Lorenzut <patrick.lorenzut@gmail.com>
2020-06-19 11:01:53 -0700

    Simpler, better portfolio grid.

--- components/PortfolioGrid.vue
+++ components/PortfolioGrid.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="container mt-24">
-      <div class="max-w-md md:max-w-6xl mx-auto mb-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <nuxt-link v-for="project in projects" :key="project.slug" class="col-span-1 relative block bg-purple-100 border border-purple-200 hover:bg-yellow-200 py-2 px-4 pt-3" :to="project.slug + '#giggle'">          
-          <p class="mb-1"><strong>{{ project.title }}</strong></p>
-          <img v-if="project.image" :src="project.image" :alt="project.title" class=""/>
-          <p class="mt-3 leading-3">{{ project.subtitle }}</p>
-          <strong class="block text-150 -mt-2 text-purple-600">&rarr;</strong>
+      <div class="max-w-md md:max-w-4xl mx-auto mb-4">
+        <nuxt-link v-for="project in projects" :key="project.slug" class="mb-16 grid grid-cols-1 md:grid-cols-2 gap-6 relative block hover:bg-yellow-200 p-4" :to="project.slug + '#giggle'">                    
+          <span class="col-span-1">
+            <img v-if="project.image" :src="project.image" :alt="project.title" class=""/>
+          </span>
+          <span class="col-span-1 relative">
+            <p class="mb-1"><strong>{{ project.title }}</strong></p>
+            <p class="mt-3 leading-3">{{ project.description }}</p>
+            <strong class="block text-150">&rarr;</strong>
+          </span>
         </nuxt-link>
       </div>       
     </div>
