008cd524b17ed733b0cbc852f187ad1f21a3b437
Patrick Lorenzut <patrick.lorenzut@gmail.com>
2020-06-24 21:33:37 -0700

    put resume and github links back

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,12 +2,12 @@
     <div id="giggle" class="relative lg:sticky lg:left-0 lg:top-0 lg:right-0 bg-white">
         <ul class="font-bold text-80 text-center p-4 mx-auto max-w-lg lg:max-w-none">
             <li class="mr-2 inline-block"><nuxt-link to="/">Hire Me</nuxt-link></li>
+            <!-- <li class="mr-2 inline-block"><nuxt-link to="/words/working-with-me">About</nuxt-link></li> -->
             <li class="mr-2 inline-block"><nuxt-link to="/projects">Portfolio</nuxt-link></li>   
-            <!-- <li class="mr-2 inline-block"><nuxt-link to="/words">Writing</nuxt-link></li> -->
-            <!-- <li class="mr-2 inline-block"><nuxt-link to="/words/about">About Me</nuxt-link></li> -->
+            <!-- <li class="mr-2 inline-block"><nuxt-link to="/words">Writing</nuxt-link></li>             -->
             <li class="mr-2 inline-block"><nuxt-link to="/client/login">Client Login</nuxt-link></li>
-            <!-- <li class="mr-2 inline-block"><nuxt-link to="/resume" target="_blank">Résumé</nuxt-link></li>
-            <li class="inline-block"><a href="https://github.com/patricklorenzut" target="_blank">GitHub</a></li> -->
+            <li class="mr-2 inline-block"><nuxt-link to="/resume" target="_blank">Résumé</nuxt-link></li>
+            <li class="inline-block"><a href="https://github.com/patricklorenzut" target="_blank">GitHub</a></li>
         </ul>
     </div>
 </template>
--- components/Title.vue
+++ components/Title.vue
@@ -2,7 +2,7 @@
     <header class="container max-w-5xl mt-20">
       <div class="text-center">
         <h1 class="text-purple-800 font-bold leading-1 text-200 md:text-300 mx-auto max-w-5xl" v-html="h1"></h1>
-        <h2 v-if="h2" class="mt-6 text-110 max-w-4xl mx-auto" v-html="h2"></h2>        
+        <h2 v-if="h2" v-interpolation class="mt-6 text-110 max-w-4xl mx-auto article" v-html="h2"></h2>        
       </div>
     </header>
 </template>
